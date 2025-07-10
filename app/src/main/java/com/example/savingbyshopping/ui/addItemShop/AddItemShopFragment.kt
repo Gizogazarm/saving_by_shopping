c9b634d3da376953e0dbf5ae6dc3b690809e377a
Gizogazarm <theblues11.helmi@gmail.com>
2025-07-10 09:14:38 +0700

    SUCCESS LISTADAPTER AT ADDITEMSHOPVIEWMODEL BUT LAYOUT STILL ERROR

--- app/src/main/java/com/example/savingbyshopping/ui/addItemShop/AddItemShopFragment.kt
+++ app/src/main/java/com/example/savingbyshopping/ui/addItemShop/AddItemShopFragment.kt
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import com.example.savingbyshopping.data.adapter.ListItemShopAdapter
+import com.example.savingbyshopping.data.response.ItemShop
 import com.example.savingbyshopping.databinding.FragmentAddItemShopBinding
 import com.example.savingbyshopping.ui.ViewModelFactory
 import com.example.savingbyshopping.ui.addShoppingList.AddShoppingListViewModel
@@ -37,20 +39,21 @@ class AddItemShopFragment : Fragment() {
         val idShopping = AddItemShopFragmentArgs.fromBundle(arguments as Bundle).idShopping
 
         with(binding) {
-            shoppingViewModel.ambilShoppingListDenganId(idShopping)
-                .observe(viewLifecycleOwner) { shoppingList ->
-                    tvToko1.text = shoppingList.namaToko
-                    tvEmail.text = shoppingList.email
-                    tvDate.text = shoppingList.tanggalTransaksi
-                    tvAmountTotalShopping.text = shoppingList.totalBelanja
-                    tvAmountTotalSaving.text = shoppingList.totalDiskon
-                    Log.d("Data Shopping", "idShopping: $idShopping ")
-
-                    btnAddItem.setOnClickListener {
-                        val action = AddItemShopFragmentDirections.actionAddItemShopFragmentToDialogAddItemShopFragment(idShopping)
-                        view.findNavController().navigate(action)
-                    }
+            shoppingViewModel.ambilShoppingListDenganItemShopById(idShopping)
+                .observe(viewLifecycleOwner) {
+                    tvToko1.text = it.shoppingList.namaToko
+                    tvEmail.text = it.shoppingList.email
+                    tvDate.text = it.shoppingList.tanggalTransaksi
+                    tvAmountTotalShopping.text = it.shoppingList.totalBelanja
+                    tvAmountTotalSaving.text = it.shoppingList.totalDiskon
+                    showRv(it.itemShop)
+                    Log.d("data shopping list", "data shopping list $it")
                 }
+
+            btnAddItem.setOnClickListener {
+                val action = AddItemShopFragmentDirections.actionAddItemShopFragmentToDialogAddItemShopFragment(idShopping)
+                view.findNavController().navigate(action)
+            }
         }
     }
 
@@ -61,6 +64,20 @@ class AddItemShopFragment : Fragment() {
 
     private fun setViewModelFactory() {
         factory = ViewModelFactory.getInstance(requireActivity())
+    }
+
+    private fun showRv(itemShop: List<ItemShop>?) {
+        if (!itemShop.isNullOrEmpty()) {
+            binding.rvItemShop.visibility = View.VISIBLE
+            binding.emptyList.visibility = View.GONE
+            val adapter = ListItemShopAdapter()
+            adapter.submitList(itemShop)
+            binding.rvItemShop.adapter = adapter
+
+        } else {
+            binding.rvItemShop.visibility = View.GONE
+            binding.emptyList.visibility = View.VISIBLE
+        }
     }
 
 }
