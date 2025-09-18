c15f214f8491cf486a25828d836d18bfdcab8ed0
Gizogazarm <theblues11.helmi@gmail.com>
2025-09-18 16:24:19 +0700

    UPDATE LAYOUT DIALOGSUCCESS

--- app/src/main/java/com/example/savingbyshopping/ui/DialogSuccess.kt
+++ app/src/main/java/com/example/savingbyshopping/ui/DialogSuccess.kt
@@ -1,14 +1,14 @@
 package com.example.savingbyshopping.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.example.savingbyshopping.databinding.FragmentDialogSuccessBinding
 
 
-class DialogSuccess : Fragment() {
+class DialogSuccess : DialogFragment() {
 
     private var _binding: FragmentDialogSuccessBinding? = null
     private val binding get() = _binding!!
